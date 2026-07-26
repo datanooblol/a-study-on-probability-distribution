b88ebaf5f2355ac855736fa235b358a55d055e8b
datanooblol <data.noob.lol@gmail.com>
2026-07-26 16:12:42 +0700

    init

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from a-study-on-probability-distribution!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/probability_distributions/utils.py
@@ -0,0 +1,2 @@
+def test():
+    return "aha probability distribution"
